481a5f74dfff07a43c1966490d9301510328c236
Tushar <tushar.hasule@somaiya.edu>
2024-08-23 16:52:00 +0530

    Frontend and tailwind inisilized !

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <header className="bg-slate-500 text-stone-700">
+        <h1>Welcome to the React App!</h1>
+        <p>This is a simple React app.</p>
+      </header>
+    </>
+  )
+}
+
+export default App
